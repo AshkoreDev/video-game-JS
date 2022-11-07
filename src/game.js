babbb98f5e95d05603501a34a085f9cd10b9c083
Aracely Alba @AshkoreDev <aracelyalba8@gmail.com>
2022-11-07 22:55:12 +0100

    Added canvas config

--- src/game.js
+++ src/game.js
@@ -1,1 +1,24 @@
-import * from './maps.js';
+import './maps.js';
+
+const canvas = document.getElementById('game');
+const game = canvas.getContext('2d');
+
+function startGame() {
+
+	// Definir donde comienza el trazo (x,y,width,height)
+	// game.fillRect(0,10,100,100);
+
+	// Borrador
+	// game.clearRect(0,0,100,50);
+
+	// Estilos del texto, debe ir antes del texto
+	game.fillStyle = 'purple';
+	game.font = '25px Verdana';
+	game.textAlign = 'center';
+
+	// Insertar texto ('texto',x,y)
+	game.fillText('Game',140,50);
+
+}
+
+window.addEventListener('load', startGame);
