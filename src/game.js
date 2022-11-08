b34f2aac0a84632d9eecc51f0bb963008c1b6df0
Aracely Alba @AshkoreDev <aracelyalba8@gmail.com>
2022-11-08 12:27:43 +0100

    Added responsive JS logic

--- src/game.js
+++ src/game.js
@@ -3,29 +3,34 @@ import { emojis } from './maps.js';
 const canvas = document.getElementById('game');
 const game = canvas.getContext('2d');
 
-function startGame() {
+let canvasSize;
+let elementsSize;
 
-	let canvasSize;
-	
+function setCanvasSize() {
+
 	(window.innerHeight > window.innerWidth)
 		?	canvasSize = window.innerWidth * 0.75
 		:	canvasSize = window.innerHeight * 0.75;
 
-	const elementsSize = canvasSize / 10;
-	
 	canvas.setAttribute('width', canvasSize);
 	canvas.setAttribute('height', canvasSize);
 
+	elementsSize = canvasSize / 10;
 	console.log({canvasSize,elementsSize});
+
+	startGame();
+}
+
+function startGame() {
 
 	game.fillStyle = 'purple';
 	game.font = elementsSize + 'px Verdana';
 	game.textAlign = 'center';
 
 	for(let i=1; i<=10; i++) {
-		game.fillText(emojis['X'], elementsSize, elementsSize*i);
+		game.fillText(emojis['X'], elementsSize, elementsSize * i);
 	}
-
 }
 
-window.addEventListener('load', startGame);
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
